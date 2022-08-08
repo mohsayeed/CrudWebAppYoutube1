62986f3bffe87c6f09153268c09d66a1d7c6fa4e
mohsayeed <cs19btech11004@iith.ac.in>
2022-08-08 20:03:32 +0530

    crud app 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ constructor(private http:HttpClient){
+ }
+
+  getApiUrl = 'https://localhost:7172/api/StudentDetails'
   title = 'learnprgm';
+  public details:any;
+  ngOnInit(){
+    this.http.get<any>(this.getApiUrl).subscribe((response)=>{
+      console.log(response)
+      this.details = response
+    })
+  }
+
 }
